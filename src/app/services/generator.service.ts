6e3bd3e38f3e979923ce5953d912637d8b7089db
a.chuevsky <a.chuevsky@itransition.com>
2021-07-18 00:13:56 +0300

    Add stylelint and fix stylelint comments.

--- src/app/services/generator.service.ts
+++ src/app/services/generator.service.ts
@@ -4,7 +4,6 @@ import { InterfaceGeneratorService } from './interface-generator.service';
 
 @Injectable()
 export class GeneratorService {
-
   constructor(public mockGeneratorService: MockGeneratorService, public interfaceGeneratorService: InterfaceGeneratorService) { }
 
   generate(isInterfaceToMockMode: boolean) {
